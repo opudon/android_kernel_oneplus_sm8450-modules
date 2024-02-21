e2b02bd0d487d95c03c428eebeae18574f356ba1
John Galt <johngaltfirstrun@gmail.com>
2024-02-21 07:57:12 +0000

    display: send POWERDOWN_CUST notifier event for LP1/LP2
    We are using UNBLANK_CUST for dpms on for coming out of LP1/2, we need
    to set POWERDOWN_CUST for entering.
    
    Fixes: touchscreen gestures on AOD, touchscreen entering suspend on AOD.
    
    Note: It takes ~5 seconds from entering AOD for panel to enter low power
    state.
    
    [Vishalcj17: adapt to OPLUS 5.4]
    [Maitreya25: adapt to OPLUS 5.10 by dropping drm_panel_notifier_call_chain]
    Change-Id: Ica9db924aace28fec5c90a3af1ee506b6c35e9ea
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Signed-off-by: Maitreya25 <maitreyapatni25@gmail.com>

--- qcom/opensource/display-drivers/msm/dsi/dsi_display.c
+++ qcom/opensource/display-drivers/msm/dsi/dsi_display.c
@@ -1550,6 +1550,8 @@ int dsi_display_set_power(struct drm_connector *connector,
 		int power_mode, void *disp)
 {
 	struct dsi_display *display = disp;
+	struct msm_drm_notifier notifier_data;
+	int blank;
 	int rc = 0;
 
 	if (!display || !display->panel) {
@@ -1560,9 +1562,17 @@ int dsi_display_set_power(struct drm_connector *connector,
 	switch (power_mode) {
 	case SDE_MODE_DPMS_LP1:
 		rc = dsi_panel_set_lp1(display->panel);
+		blank = MSM_DRM_BLANK_POWERDOWN;
+		notifier_data.data = &blank;
+		notifier_data.id = 0;
+		msm_drm_notifier_call_chain(MSM_DRM_EARLY_EVENT_BLANK, &notifier_data);
 		break;
 	case SDE_MODE_DPMS_LP2:
 		rc = dsi_panel_set_lp2(display->panel);
+		blank = MSM_DRM_BLANK_POWERDOWN;
+		notifier_data.data = &blank;
+		notifier_data.id = 0;
+		msm_drm_notifier_call_chain(MSM_DRM_EARLY_EVENT_BLANK, &notifier_data);
 		break;
 	case SDE_MODE_DPMS_ON:
 		if ((display->panel->power_mode == SDE_MODE_DPMS_LP1) ||
